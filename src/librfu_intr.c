af9001a5f6597541a31904f17d65407e74e6f872
Ydnt24681055 <torbatiidan241@gmail.com>
2026-06-03 22:41:51 +0300

    Fix Callback_Dummy_M signature alignment in librfu_intr

--- src/librfu_intr.c
+++ src/librfu_intr.c
@@ -7,7 +7,7 @@ static u16 handshake_wait(u16 slot);
 static void STWI_set_timer_in_RAM(u8 count);
 static void STWI_stop_timer_in_RAM(void);
 static void STWI_init_slave(void);
-static void Callback_Dummy_M(int reqCommandId, int error, void (*callbackM)());
+static void Callback_Dummy_M(int reqCommandId, int error, void (*callbackM)(...));
 static void Callback_Dummy_S(u16 reqCommandId, void (*callbackS)(u16));
 static void Callback_Dummy_ID(void (*callbackId)(void));
 
@@ -388,7 +388,7 @@ static void STWI_init_slave(void)
 }
 
 NAKED
-static void Callback_Dummy_M(int reqCommandId, int error, void (*callbackM)())
+static void Callback_Dummy_M(int reqCommandId, int error, void (*callbackM)(...))
 {
     asm("bx r2");
 }
